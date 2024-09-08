80f95e57a5b4ac2fce2ccb1b3a5f23f96e31d3ee
AbhimanyuKoushik <abhimanyukoushikg@gmail.com>
2024-09-08 21:01:29 +0530

    commit

--- Problem5/codes/code.c
+++ Problem5/codes/code.c
@@ -9,36 +9,29 @@
 #include "libs/matfun.h"
 #include "libs/geofun.h"
 
-void point_gen(FILE *fptr, double **A, double **direction_vector, int no_rows, int no_col, int num_points) {
+void point_gen(FILE *fptr, double **center, double **point_on_circle, int no_rows, int no_col, int num_points) {
     double **output;
-    for (int i = -num_points/2; i <= num_points/2; i++) {
-        output = Matadd(A, Matscale(direction_vector,no_rows,no_col,(double) i/num_points), no_rows, no_col);
+    double **dir_vector = Matsub(point_on_circle, center, no_rows, no_col);
+    for (int i = 0; i <= num_points; i++) {
+        output = Matadd(Matmul(rotMat((i * 2 * M_PI) / num_points), dir_vector, 2, 2, 1), center, no_rows, no_col);
         fprintf(fptr, "%lf,%lf\n", output[0][0], output[1][0]);
         freeMat(output, no_rows);
     }
+    freeMat(dir_vector, no_rows);  // Free direction vector
 }
 
-int main(){
+int main() {
     int x1 = 2, y1 = -4, x2 = 3, y2 = 8;
     int m = 2, n = 1;
-    double **A, **B, **midpoint, **s_ab, **bisectorABMidpoint;
+    double **A, **P;
 
     A = createMat(m, n);
-    B = createMat(m, n);
-    midpoint = createMat(m, n);
+    P = createMat(m, n);
 
     A[0][0] = x1;
     A[1][0] = y1;
-    B[0][0] = x2;
-    B[1][0] = y2;
-
-    // Calculate the midpoint of AB
-    midpoint = Matadd(A, B, m, n);
-    midpoint = Matscale(midpoint, m, n, 1.0/2);
-
-    // Calculate the vector AB and then the perpendicular bisector vector
-    s_ab = Matsub(B, A, m, n);
-    bisectorABMidpoint = normVec(s_ab);
+    P[0][0] = x2;
+    P[1][0] = y2;
 
     // Open file to write points
     FILE *fptr;
@@ -48,34 +41,35 @@ int main(){
         return 1;
     }
 
-    // Generate points on the perpendicular bisector
-    point_gen(fptr, midpoint, bisectorABMidpoint, m, n, 20);
-    
-    double **reqpoint1;
-    reqpoint1=createMat(m,n);
-    reqpoint1[0][0]=-10;
-    reqpoint1[1][0]=-3;
-    
-    double **reqpoint2;
-    reqpoint2=createMat(m,n);
-    reqpoint2[0][0]=-10;
-    reqpoint2[1][0]=-5;
-    
-    if((Matnorm(Matsub(A,reqpoint1,m,n),m)==Matnorm(Matsub(B,reqpoint1,m,n),m))&&(Matnorm(Matsub(A,reqpoint1,m,n),m)==Matnorm(Matsub(B,reqpoint1,m,n),m))){
-    	fprintf(fptr,"Point is equidistant from the given points");
+    // Generate points on the circle
+    point_gen(fptr, A, P, m, n, 100);
+
+    // Checking equidistant points
+    double **reqpoint1, **reqpoint2;
+    reqpoint1 = createMat(m, n);
+    reqpoint2 = createMat(m, n);
+
+    reqpoint1[0][0] = -10;
+    reqpoint1[1][0] = -3;
+
+    reqpoint2[0][0] = -10;
+    reqpoint2[1][0] = -5;
+
+    // Check if the point is equidistant from A and P
+    if ((Matnorm(Matsub(A, reqpoint1, m, n), m) == Matnorm(Matsub(P, reqpoint1, m, n), m)) &&
+        (Matnorm(Matsub(A, reqpoint2, m, n), m) == Matnorm(Matsub(P, reqpoint2, m, n), m))) {
+        fprintf(fptr, "Point is equidistant from the given points\n");
     }
-	
+
     // Close the file
     fclose(fptr);
 
     // Free all allocated memory
-    freeMat(A,m);
-    freeMat(B,m);
-    freeMat(midpoint,m);
-    freeMat(s_ab,m);
-    freeMat(bisectorABMidpoint,m);
-    freeMat(reqpoint1,m);
-    freeMat(reqpoint2,m);
+    freeMat(A, m);
+    freeMat(P, m);
+    freeMat(reqpoint1, m);
+    freeMat(reqpoint2, m);
+
     return 0;
 }
 
