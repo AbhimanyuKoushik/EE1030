d771699389074a36205c3a61bea7581075a2c8d8
AbhimanyuKoushik <abhimanyukoushikg@gmail.com>
2024-09-14 15:06:02 +0530

    commit

--- Problem7/codes/code.c
+++ Problem7/codes/code.c
@@ -17,55 +17,61 @@ void point_gen(FILE *fptr, double **A, double **B, int no_rows, int no_cols, int
 }
 
 int main() {
-    double x, y, z, x1, y1, z1, x2, y2, z2, x3, y3, z3;
-//    scanf("%lf %lf %lf %lf %lf %lf", &x1, &y1, &x2, &y2, &x3, &y3);
+    //coordinates of tail and head of vector (1,1,1)^T
+    double x, y, z, x1, y1, z1;
     int m = 3, n = 1;
     
     x = 1; y = 1; z = 1;
+    x1 = 0; y1 = 0; z1 = 0;
     
+    // creating a the vector r
     double **R = createMat(m,n);
     R[0][0] = x;
     R[1][0] = y;
     R[2][0] = z;
+    
+    // finding the scaling values
     double c1 = (2*sqrt(3))/(Matnorm(R,m));
     double c2 = -(2*sqrt(3))/(Matnorm(R,m));
     
-    x1 = 0; y1 = 0; z1 = 0;
-    x2 = c1*x; y2 = c1*y; z2 = c1*z;
-    x3 = c2*x; y3 = c2*y; z3 = c2*z;
-    
-
+    // creating vectors which satisfy the given conditions
     double **A = createMat(m, n);
     double **B = createMat(m, n);
     double **C = createMat(m, n);
-
+    
+    // A is the origin
     A[0][0] = x1;
     A[1][0] = y1;
     A[2][0] = z1;
-    B[0][0] = x2;
-    B[1][0] = y2;
-    B[2][0] = z2;
-    C[0][0] = x3;
-    C[1][0] = y3;
-    C[2][0] = z3;
-
+    
+    // multiplying vector r with given scaling factors
+    B = Matscale(R,m,n,c1);
+    C = Matscale(R,m,n,c2);
+    
+    // opening the file
     FILE *fptr;
     fptr = fopen("vector.dat", "w");
     if (fptr == NULL) {
         printf("Error opening file!\n");
         return 1;
     }
-
+    
+    // generating points 
     point_gen(fptr, A, B, 3, 1, 20);
     point_gen(fptr, A, C, 3, 1, 20);
+    
+    // checking the length of the vector
     double length = Matnorm(Matsub(B,A,m,n),m);
     
+    // free matrices
     freeMat(A,m);
     freeMat(B,m);
     freeMat(C,m);
     freeMat(R,m);
     
     fprintf(fptr, "The length of vector is %lf", length);
+    
+    // closing the file
     fclose(fptr);
     return 0;
 }
