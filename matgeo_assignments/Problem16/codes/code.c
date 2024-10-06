d36dd0e713f99f678b719ed873513d621f8a7999
AbhimanyuKoushik <abhimanyukoushikg@gmail.com>
2024-10-06 20:03:36 +0530

    commit

--- matgeo_assignments/Problem16/codes/code.c
+++ matgeo_assignments/Problem16/codes/code.c
@@ -9,142 +9,120 @@
 #include "libs/matfun.h"
 #include "libs/geofun.h"
 
-void parab_x2_4ay_gen(FILE *fptr, double **x1, double **x2, double a, int num_points) {
-    
-    double tinit = (x1[0][0] / (2 * a));
-    double tfinal = (x2[0][0] / (2 * a));
-    double **point = createMat(2, 1);
+typedef struct {
+    double m[2][2];
+} Matrix2x2;
+
+typedef struct {
+    double v[2];
+} Vector2x1;
+
+void parab_x2_4ay_gen(FILE *fptr, Vector2x1 *p1, Vector2x1 *p2, double a, int num_points) {
+    double tinit = p1->v[0] / (2 * a);
+    double tfinal = p2->v[0] / (2 * a);
+
+    Vector2x1 point;
+
     if (tfinal > tinit) {
-        for (double i = tinit; i <= tfinal+(2.0/num_points); i += 2.0 / num_points) {
-            point[0][0] = 2 * a * i;
-            point[1][0] = a * i * i;
-            fprintf(fptr, "%lf,%lf\n", point[0][0], point[1][0]);  // Don't forget newline for correct formatting
-    }
-}
-    else{
-    	for (double i = tfinal; i <= tinit+(2.0/num_points); i += 2.0 / num_points) {
-            point[0][0] = 2 * a * i;
-            point[1][0] = a * i * i;
-            fprintf(fptr, "%lf,%lf\n", point[0][0], point[1][0]);  // Don't forget newline for correct formatting
+        for (double i = tinit; i <= tfinal + (2.0 / num_points); i += 2.0 / num_points) {
+            point.v[0] = 2 * a * i;
+            point.v[1] = a * i * i;
+            fprintf(fptr, "%lf,%lf\n", point.v[0], point.v[1]);
+        }
+    } else {
+        for (double i = tfinal; i <= tinit + (2.0 / num_points); i += 2.0 / num_points) {
+            point.v[0] = 2 * a * i;
+            point.v[1] = a * i * i;
+            fprintf(fptr, "%lf,%lf\n", point.v[0], point.v[1]);
         }
     }
-    freeMat(point, 2);  // Free point vector
 }
 
-double** intersect_of_parab_line(double **V,double **u,double f,double **point_on_line,double **dir){
-	double m1 = dir[0][0];
-	double m2 = dir[1][0];
-	double V1 = V[0][0];
-	double V2 = V[1][0];
-	double V3 = V[0][1];
-	double V4 = V[1][1];
-	double u1 = u[0][0];
-	double u2 = u[1][0];
-	double h1 = point_on_line[0][0];
-	double h2 = point_on_line[1][0];
-	
-	double A = V1*m1*m1+(V2+V3)*m1*m2+V4*m2*m2;
-	double B = 2*(m1*(V1*h1+V2*h2+u1)+m2*(V3*h1+V4*h2+u2));
-	double C = h1*(V1*h1+V2*h2)+h2*(V3*h1+V4*h2)+2*(u1*h1+u2*h2)+f;
-	
-	if(B*B-4*A*C<0){
-		return NULL;
-	}
-	else if(B*B-4*A*C==0){
-		double k;
-		double **intersect;
-		
-		k = (-B)/(2*A);
-		intersect = createMat(2,1);
-		intersect = Matadd(point_on_line, Matscale(dir,2,1,k),2,1);
-		return intersect;
-	}
-	else if(B*B-4*A*C>0){
-		double k1 = (-B+sqrt(B*B-4*A*C))/(2*A), k2 = (-B-sqrt(B*B-4*A*C))/(2*A);
-		double **intersect1 = createMat(2,1);
-		double **intersect2 = createMat(2,1);
-		double **intersect = createMat(2,2);
-		intersect1 = Matadd(point_on_line, Matscale(dir,2,1,k1),2,1);
-		intersect2 = Matadd(point_on_line, Matscale(dir,2,1,k2),2,1);
-		intersect[0][0] = intersect1[0][0];
-		intersect[1][0] = intersect1[1][0];
-		intersect[0][1] = intersect2[0][0];
-		intersect[1][1] = intersect2[1][0];
-		return intersect;
-	}
+Vector2x1* intersect_of_parab_line(Matrix2x2 *V, Vector2x1 *u, double f, Vector2x1 *point_on_line, Vector2x1 *dir) {
+    double m1 = dir->v[0];
+    double m2 = dir->v[1];
+
+    double V1 = V->m[0][0];
+    double V2 = V->m[1][0];
+    double V3 = V->m[0][1];
+    double V4 = V->m[1][1];
+
+    double u1 = u->v[0];
+    double u2 = u->v[1];
+
+    double h1 = point_on_line->v[0];
+    double h2 = point_on_line->v[1];
+
+    double A = V1 * m1 * m1 + (V2 + V3) * m1 * m2 + V4 * m2 * m2;
+    double B = 2 * (m1 * (V1 * h1 + V2 * h2 + u1) + m2 * (V3 * h1 + V4 * h2 + u2));
+    double C = h1 * (V1 * h1 + V2 * h2) + h2 * (V3 * h1 + V4 * h2) + 2 * (u1 * h1 + u2 * h2) + f;
+
+    if (B * B - 4 * A * C < 0) {
+        return NULL;
+    } else {
+        Vector2x1 *intersection = malloc(2 * sizeof(Vector2x1));
+        if (B * B - 4 * A * C == 0) {
+            double k = -B / (2 * A);
+            intersection[0].v[0] = point_on_line->v[0] + k * dir->v[0];
+            intersection[0].v[1] = point_on_line->v[1] + k * dir->v[1];
+            return intersection;
+        } else {
+            double k1 = (-B + sqrt(B * B - 4 * A * C)) / (2 * A);
+            double k2 = (-B - sqrt(B * B - 4 * A * C)) / (2 * A);
+
+            intersection[0].v[0] = point_on_line->v[0] + k1 * dir->v[0];
+            intersection[0].v[1] = point_on_line->v[1] + k1 * dir->v[1];
+
+            intersection[1].v[0] = point_on_line->v[0] + k2 * dir->v[0];
+            intersection[1].v[1] = point_on_line->v[1] + k2 * dir->v[1];
+
+            return intersection;
+        }
+    }
 }
 
-double square(double x)
-{
-	return x*x  ;
-}
-
-double identity(double x)
-{
-	return x;
-}
-
-double area(double (*func)(double), double lower_limit, double upper_limit)
-{
+double area(double (*func)(double), double lower_limit, double upper_limit) {
     double sum = 0;
-    for (double i = lower_limit; i <= upper_limit; i += 1e-3)
-    {
-        //sum += (1.0/2.0)*(func(i)+func(i-1e-3))* 1e-3;
-        sum += func(i)*1e-3;
+    for (double i = lower_limit; i <= upper_limit; i += 1e-3) {
+        sum += func(i) * 1e-3;
     }
     return sum;
 }
 
-int main(){
-    double **V = createMat(2,2), **u = createMat(2,1), f, **point_on_line = createMat(2,1), **dir = createMat(2,1);
-    
-    V[0][0] = 1;
-    V[1][0] = 0;
-    V[0][1] = 0;
-    V[1][1] = 0;
-    
-    u[0][0] = 0;
-    u[1][0] = -1.0/2.0;
-    
-    f = 0;
-    
-    point_on_line[0][0] = 0;
-    point_on_line[1][0] = 0;
-    
-    dir[0][0] = 1;
-    dir[1][0] = 1;
-    
-    double **intersection = createMat(2,2);
-    intersection = intersect_of_parab_line(V,u,f,point_on_line,dir);
-    
-    double **x1 = createMat(2,1);
-    double **x2 = createMat(2,1);
-    x1[0][0] = intersection[0][0];
-    x1[1][0] = intersection[1][0];
-    x2[0][0] = intersection[0][1];
-    x2[1][0] = intersection[1][1];
-    
-    // Open file to write points
-    FILE *fptr;
-    fptr = fopen("parab.dat", "w");
+double square(double x) {
+    return x * x;
+}
+
+double identity(double x) {
+    return x;
+}
+
+int main() {
+    Matrix2x2 V = {{{1, 0}, {0, 0}}};
+    Vector2x1 u = {{0, -0.5}};
+    double f = 0;
+
+    Vector2x1 point_on_line = {{0, 0}};
+    Vector2x1 dir = {{1, 1}};
+
+    Vector2x1 *intersection = intersect_of_parab_line(&V, &u, f, &point_on_line, &dir);
+
+    Vector2x1 p1 = {{intersection[0].v[0], intersection[0].v[1]}};
+    Vector2x1 p2 = {{intersection[1].v[0], intersection[1].v[1]}};
+
+    FILE *fptr = fopen("parab.dat", "w");
     if (fptr == NULL) {
         printf("Error opening file!\n");
         return 1;
     }
 
-    parab_x2_4ay_gen(fptr,x1,x2,-u[1][0]/2,200);
-    
-    fprintf(fptr,"The area between the given curve and line is %lf",area(identity,0,1)-area(square,0,1));
-   
-    
-    // Close the file
+    parab_x2_4ay_gen(fptr, &p1, &p2, -u.v[1] / 2, 200);
+
+    fprintf(fptr, "The area between the given curve and line is %lf", area(identity, 0, 1) - area(square, 0, 1));
+
     fclose(fptr);
 
-    // Free all allocated memory
-    freeMat(x1, 2);
-    freeMat(x2, 2);
-    freeMat(dir,2);
-    freeMat(point_on_line,2);
+    free(intersection);
 
     return 0;
 }
